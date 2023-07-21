4070b452f5a1060ae8fdf53ce906b43471c5ff69
Juli Scapucin <juli.scapucin@deptagency.com>
2023-07-21 21:35:35 +0200

    TOS: fix parallax images

--- src/components/Parallax.tsx
+++ src/components/Parallax.tsx
@@ -16,10 +16,7 @@ function Parallax() {
         data-animation-speed="-1"
       >
         <figure className="parallax__image-inner-container">
-          <img
-            className="parallax__image"
-            src="https://garoaskincare.com/home/seasons-1.webp"
-          />
+          <img className="parallax__image" src="./image.jpeg" />
         </figure>
       </div>
       <div
@@ -28,10 +25,7 @@ function Parallax() {
         data-animation-speed="-2"
       >
         <figure className="parallax__image-inner-container">
-          <img
-            className="parallax__image"
-            src="https://garoaskincare.com/home/seasons-2.webp"
-          />
+          <img className="parallax__image" src="./image.jpeg" />
         </figure>
       </div>
       <div
@@ -40,10 +34,7 @@ function Parallax() {
         data-animation-speed="-3"
       >
         <figure className="parallax__image-inner-container">
-          <img
-            className="parallax__image"
-            src="https://garoaskincare.com/home/seasons-3.webp"
-          />
+          <img className="parallax__image" src="./image.jpeg" />
         </figure>
       </div>
       <div
@@ -52,10 +43,7 @@ function Parallax() {
         data-animation-speed="0"
       >
         <figure className="parallax__image-inner-container">
-          <img
-            className="parallax__image"
-            src="https://garoaskincare.com/home/seasons-4.webp"
-          />
+          <img className="parallax__image" src="./image.jpeg" />
         </figure>
       </div>
     </section>
